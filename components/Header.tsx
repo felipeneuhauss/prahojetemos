2696b02c5d683b24f834ca02bc614a7a620f5096
Felipe Araujo <FelipeAraujo.DECSKILL@edp.com>
2022-06-21 16:21:29 +0100

    fix: build fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,38 +1,44 @@
-/** @jsxRuntime automatic */
 /** @jsxImportSource theme-ui */
+/** @jsxRuntime automatic */
 
 import { Box, Flex } from 'theme-ui';
 import MainTrends from './MainTrends';
 
-function Header() {
-  return (
-    <Box sx={{
-      bg: 'primary',
-      width: '100%',
-      px: [20, null, null, 0],
-      py: [20, null, null, 80],
-    }}
+const Header = () => (
+  <Box sx={{
+    bg: 'primary',
+    width: '100%',
+    px: [20, null, null, 0],
+    py: [20, null, null, 80],
+  }}
+  >
+    <Flex
+      sx={{
+        width: ['100%', null, null, 1024],
+        margin: '0 auto',
+        flexDirection: ['column', null, null, 'row'],
+        gap: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
     >
-      <Flex
-        sx={{
-          width: ['100%', null, null, 1024],
-          margin: '0 auto',
-          flexDirection: ['column', null, null, 'row'],
-          gap: 20,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+      <Flex sx={{
+        color: 'white',
+        width: ['100%', null, null, '50%'],
+        flexDirection: 'column',
+      }}
       >
-        <Flex sx={{ color: 'white', width: ['100%', null, null, '50%'], flexDirection: 'column' }}>
-          <h1>As notícias mais pesquisadas da net nas últimas 24h</h1>
-          <h2>Aqui você encontra as melhores materiais dos termos mais pesquisados da net nas últimas 24 horas.</h2>
-        </Flex>
-        <Flex sx={{ width: ['100%', null, null, '50%'] }}>
-          <MainTrends />
-        </Flex>
+        <h1>As notícias mais pesquisadas da net nas últimas 24h</h1>
+        <h2>
+          Aqui você encontra as melhores materiais dos termos
+          mais pesquisados da net nas últimas 24 horas.
+        </h2>
       </Flex>
-    </Box>
-  );
-}
+      <Flex sx={{ width: ['100%', null, null, '50%'] }}>
+        <MainTrends />
+      </Flex>
+    </Flex>
+  </Box>
+);
 
 export default Header;
--- components/MainTrends.tsx
+++ components/MainTrends.tsx
@@ -1,46 +1,47 @@
 /** @jsxRuntime automatic */
 /** @jsxImportSource theme-ui */
+
 import React from 'react';
-import { useMainTrends } from 'contexts/MainTrendsProvider';
-import { Flex, Link, Text } from 'theme-ui';
+import { Flex } from 'theme-ui';
 import slugify from 'slugify';
+import ellipsis from 'shared/helpers/ellipsis';
+import { useMainTrends } from 'contexts/MainTrendsProvider';
 
 type TrendLinkProps = {
-  trend: string;
+    trend: string;
 }
 
-const TrendLink: React.FC<TrendLinkProps> = ({ trend }: TrendLinkProps) => <li><Link href={`#${slugify(trend.toLowerCase())}`}>{trend}</Link></li>;
+const TrendLink: React.FC<TrendLinkProps> = ({ trend }: { trend: string }) => <li><a href={`#${slugify(trend.toLowerCase())}`}>{ellipsis(trend, 50)}</a></li>;
 
-function MainTrends() {
+const MainTrends: React.FC = () => {
   const { mainTrends } = useMainTrends();
-
   return (
     <Flex sx={{
-      border: '1px solid #e5e5e5',
+      border: '1px solid #E5E5E5',
       bg: 'white',
       flexDirection: 'column',
       borderRadius: 4,
       pb: 20,
-      height: 360,
+      height: '360px',
       overflowY: 'auto',
     }}
     >
-      <Text as="h2" sx={{ px: 24 }}>Principais termos pesquisados</Text>
+      <h2 sx={{ px: 24 }}>Principais termos pesquisados</h2>
       <ul sx={{
         px: 28,
-        '> li': {
+        '> li ': {
           listStyle: 'none',
-          borderBottom: '1px solid #e5e5e5',
+          borderBottom: '1px solid #E5E5E5',
           '> a:hover': {
             textDecoration: 'underline',
           },
         },
       }}
       >
-        {mainTrends && mainTrends?.map((trend: string) => <TrendLink trend={trend} key={slugify(`link-${trend}`)} />)}
+        {mainTrends && mainTrends?.map((trend: string) => <TrendLink trend={trend} key={`trend-link-${slugify(trend)}`} />)}
       </ul>
     </Flex>
   );
-}
+};
 
 export default MainTrends;
