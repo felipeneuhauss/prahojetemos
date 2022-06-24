963aabcdcf357087d1b6da0a1fcf47edb1d9bf40
Felipe Araujo <FelipeAraujo.DECSKILL@edp.com>
2022-06-24 07:08:09 +0100

    feat: nextjs image tag

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      't0.gstatic.com',
+      't1.gstatic.com',
+      't2.gstatic.com',
+      't3.gstatic.com',
+      't4.gstatic.com',
+      't5.gstatic.com',
+      't6.gstatic.com',
+      't7.gstatic.com',
+      't8.gstatic.com',
+      't9.gstatic.com',
+      't10.gstatic.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
